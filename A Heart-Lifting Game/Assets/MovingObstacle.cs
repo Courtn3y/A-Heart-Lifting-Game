c0c56a97fb72ff9e7244097afefd623a24fb78dd
Dan <daniel9.bennett@live.uwe.ac.uk>
2020-01-31 23:08:19 +0000

    Obstacles can move
    They can move between many different points in a linear and curved manner. Nothing else has been added yet.

---
+++ A Heart-Lifting Game/Assets/MovingObstacle.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+
+    public ObjectRails rail;
+    public PlayMode mode;
+
+    public float speed;
+    public float currentSpeed;
+
+    public bool isReversed;
+    public bool isLooping;
+    public bool pingPong;
+    public bool isMoving;
+    public bool autoPlay;
+
+    public bool forwards;
+    public bool backwards;
+
+    public int currentSeg;
+    private float transition;
+    public bool isCompleted;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forwards)
+        {
+            speed = currentSpeed;
+            isReversed = false;
+        }
+        else if (backwards)
+        {
+            speed = currentSpeed;
+            isReversed = true;
+        }
+        else if (autoPlay)
+        {
+            speed = currentSpeed;
+        }
+        else
+        {
+            speed = 0;
+        }
+
+        if (!rail)
+        {
+            return;
+        }
+
+        if (!isCompleted)
+        {
+            Play(!isReversed);
+        }
+    }
+
+    private void Play(bool forward = true)
+    {
+
+
+        float m = (rail.nodes[currentSeg + 1].position - rail.nodes[currentSeg].position).magnitude;
+        float s = (Time.deltaTime * 1 / m) * speed;
+        transition += (forward) ? s : -s;
+
+
+        // Checks to see if the current segment has hit the next, and then sets the transition to 0, and carries on
+        // with moving forward
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+            if (currentSeg == rail.nodes.Length - 1)
+            {
+                if (isLooping)
+                {
+                    if (pingPong)
+                    {
+                        transition = 1;
+                        currentSeg = rail.nodes.Length - 2;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = 0;
+                    }
+                }
+            }
+        }
+        // Checks to see if current segment has hit the previous part, and then sets the transition to 1 and scrolls back
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+
+            if (currentSeg == -1)
+            {
+                if (isLooping)
+                {
+                    if (pingPong)
+                    {
+                        transition = 0;
+                        currentSeg = 0;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = rail.nodes.Length - 2;
+                    }
+                }
+            }
+        }
+
+        // Sends information to the rail and allows the platform to move along the spline 
+        transform.position = rail.PositionOnRail(currentSeg, transition, mode);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        /* Check what is colliding
+           if (dragItem)
+              Then drag with obstacle
+              Or reset item
+              or destroy item
+         */
+    }
+}
---
+++ A Heart-Lifting Game/Assets/ObjectRails.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum PlayMode
+{
+    Linear,
+    Catmull,
+}
+
+[ExecuteInEditMode]
+public class ObjectRails : MonoBehaviour
+{
+
+    
+
+
+    public GameObject obstacle;
+    public Transform[] nodes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    
+    public Vector2 PositionOnRail(int seg, float ratio, PlayMode mode)
+    {
+        switch (mode)
+        {
+            default:
+            case PlayMode.Linear:
+                return LinearPosition(seg, ratio);
+            case PlayMode.Catmull:
+                return CatmullPosition(seg, ratio);
+        }
+    }
+
+    public Vector2 LinearPosition(int seg, float ratio)
+    {
+        Vector2 p1 = nodes[seg].position;
+        Vector2 p2 = nodes[seg + 1].position;
+
+        return Vector2.Lerp(p1, p2, ratio);
+    }
+
+    public Vector2 CatmullPosition(int seg, float ratio)
+    {
+        Vector2 p1, p2, p3, p4;
+
+        if (seg == 0)
+        {
+            p1 = nodes[seg].position;
+            p2 = p1;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+        else if (seg == nodes.Length - 2)
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = p3;
+        }
+        else
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+
+        float t2 = ratio * ratio;
+        float t3 = t2 * ratio;
+
+        float x =
+            0.5f * ((2.0f * p2.x) +
+            (-p1.x + p3.x) * ratio +
+            (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2 +
+            (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
+
+        float y =
+           0.5f * ((2.0f * p2.y) +
+           (-p1.y + p3.y) * ratio +
+           (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y) * t2 +
+           (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) * t3);
+
+        return new Vector2(x, y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+}
