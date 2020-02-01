ff5859782f3e9038b50c6d92eabc6a2182defc0c
E2-Waite <E2-Waite>
2020-02-01 14:37:44 +0000

    Added directional arrow
    Added arrow that always points in the

---
+++ A Heart-Lifting Game/Assets/PointArrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointArrow : MonoBehaviour
+{
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = GameObject.FindGameObjectWithTag("Goal").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = goal.position - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+
+}
