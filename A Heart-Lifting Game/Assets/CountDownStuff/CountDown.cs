db587361fb01567964e59fb5bb384755c874e479
Courtney Daniels <Courtney2.daniels@live.uwe.ac.uk>
2020-02-02 14:55:39 +0000

    UI timer Added

---
+++ A Heart-Lifting Game/Assets/CountDownStuff/CountDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 20f;
+
+    [SerializeField] Text timerNumber;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerNumber.text = currentTime.ToString("0");
+
+        if (currentTime <= 5)
+        {
+            timerNumber.color = Color.red;
+        }
+
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
+
+    }
+}
