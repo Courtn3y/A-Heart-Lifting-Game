553e7b2380820d7ebb1fdb8c343458b016746589
Dan <daniel9.bennett@live.uwe.ac.uk>
2020-02-02 11:59:20 +0000

    Scrolling background and floor
    Uses different scrolling effects

---
+++ A Heart-Lifting Game/Assets/Scripts/Background/FollowUV.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour
+{
+    MeshRenderer mesh;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Material mat = mesh.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x;
+        offset.y = transform.position.y / transform.localScale.y;
+
+        mat.mainTextureOffset = offset;
+    }
+}
